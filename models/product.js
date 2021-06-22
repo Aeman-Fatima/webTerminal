ffd4dd6619ff95f0f82e4e29d134f19b5ffbd445
Aeman Fatima <aeman098.fatima@gmail.com>
2021-06-22 10:13:19 +0500

    Add files via upload

---
+++ models/product.js
@@ -0,0 +1,9 @@
+var mongoose = require("mongoose");
+
+var productSchema = mongoose.Schema({
+  name: String,
+  price: String,
+  rating: Number
+});
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
