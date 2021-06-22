b1d29e59515ac4ae9ea8980f2a95303e8c07f108
123qwe456 <aeman098.fatima@gmail.com>
2021-06-23 03:18:56 +0500

    rollnumber

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
+  res.render("index", { title: "FA18-BSE-048" });
 });
 router.get("/cart", function (req, res, next) {
   let cart = req.cookies.cart;
